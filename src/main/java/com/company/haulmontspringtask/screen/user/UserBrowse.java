8b324e82878a88e7b046a821c4ff1523c907abac
antonhonakov <antoshkaxon@mail.ru>
2022-08-31 15:53:31 +0800

    Initial commit

---
+++ src/main/java/com/company/haulmontspringtask/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package com.company.haulmontspringtask.screen.user;
+
+import com.company.haulmontspringtask.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
---
+++ src/test/java/com/company/haulmontspringtask/HaulmontSpringTaskApplicationTests.java
@@ -0,0 +1,22 @@
+package com.company.haulmontspringtask;
+
+import com.company.haulmontspringtask.entity.User;
+import com.company.haulmontspringtask.security.DatabaseUserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HaulmontSpringTaskApplicationTests {
+    @Autowired
+    DatabaseUserRepository databaseUserRepository;
+
+    @Test
+    void checkUser() {
+        User systemUser = databaseUserRepository.getSystemUser();
+        assertEquals(systemUser.getUsername(), "system1", "System user is not correct");
+    }
+
+}
